69bef9bbece35d6c9270f92ead3912763ace8224
sinking8 <ashwinblaze111@gmail.com>
2023-02-25 15:56:57 +0530

    updated

--- backend/app/views.py
+++ backend/app/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 
+from posturedetect import Model
+
 # Create your views here.
 def index(request):
     return render(request, "pages/index.html")
+
+def detect(request):
+    image_dir = ""
+    model_inst= Model()
+    # sample response {'text': 'Not Correct Posture'}
+    return model_inst.predictions(image_dir)
